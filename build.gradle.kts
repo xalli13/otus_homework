1a19b41b8bb2827b15380d3a4e3a9026a5fd017d
Simdyankin Dmitriy <d.simdyankin@qiwi.com>
2023-03-05 23:45:19 +0600

    homework-01

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import org.gradle.api.plugins.JavaPluginExtension
+import fr.brouillard.oss.gradle.plugins.JGitverPluginExtension
+import fr.brouillard.oss.gradle.plugins.JGitverPlugin
+
+plugins {
+    idea
+    id("fr.brouillard.oss.gradle.jgitver")
+    id("io.spring.dependency-management")
+    id("org.springframework.boot") apply false
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(17)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+
+allprojects {
+    group = "ru.otus"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    val testcontainersBom: String by project
+    val protobufBom: String by project
+
+    val guava: String by project;
+
+
+    apply(plugin = "io.spring.dependency-management")
+    dependencyManagement {
+        dependencies {
+            imports {
+                mavenBom(BOM_COORDINATES)
+                mavenBom("org.testcontainers:testcontainers-bom:$testcontainersBom")
+                mavenBom("com.google.protobuf:protobuf-bom:$protobufBom")
+            }
+            dependency("com.google.guava:guava:$guava")
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing", "-Werror"))
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
+        }
+    }
+
+    plugins.apply(JavaPlugin::class.java)
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    plugins.apply(JGitverPlugin::class.java)
+    extensions.configure<JGitverPluginExtension> {
+        strategy("PATTERN")
+        nonQualifierBranches("main,master")
+        tagVersionPattern("\${v}\${<meta.DIRTY_TEXT}")
+        versionPattern(
+            "\${v}\${<meta.COMMIT_DISTANCE}\${<meta.GIT_SHA1_8}" +
+                    "\${<meta.QUALIFIED_BRANCH_NAME}\${<meta.DIRTY_TEXT}-SNAPSHOT"
+        )
+    }
+}
